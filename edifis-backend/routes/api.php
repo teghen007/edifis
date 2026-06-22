eebae0bfb494ea24562210c15cdfce2f7db41cdd
teghen007 <TEGHEN007@GMAIL.COM>
2026-06-22 17:21:56 +0100

    fix: /api/schools must bypass tenant-identification middleware (central/apex route)

--- edifis-backend/routes/api.php
+++ edifis-backend/routes/api.php
@@ -26,7 +26,9 @@ Route::get('/health', HealthController::class)->name('health');
 
 Route::get('/', [PublicWebsiteController::class, 'landing'])->name('public.landing');
 Route::post('/onboarding/request', [PublicWebsiteController::class, 'submit'])->name('public.submit');
-Route::get('/schools', [PublicWebsiteController::class, 'schools'])->name('public.schools');
+Route::get('/schools', [PublicWebsiteController::class, 'schools'])
+    ->withoutMiddleware(\Stancl\Tenancy\Middleware\InitializeTenancyByDomain::class)
+    ->name('public.schools');
 
 Route::get('/tenancy/domain-allowed', [TenancyController::class, 'domainAllowed'])
     ->withoutMiddleware(\Stancl\Tenancy\Middleware\InitializeTenancyByDomain::class)
